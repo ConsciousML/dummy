e183080627884a30873d2204abfa181b83d96520
Celine <celine.wang.etu@univ-lille.fr>
2023-04-07 13:34:55 +0200

    add trunk

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from landscape.component.tree import Tree
 from landscape.component.rock import Rock
+from landscape.component.trunk import Trunk
+
 from landscape.forest import Forest
 
 if __name__ == '__main__':
@@ -7,10 +9,11 @@ if __name__ == '__main__':
 
     rock = Rock(5, 3, '#')
     tree_o = Tree(3, 'o')
-
+    trunk = Trunk(5, '*')
     forest = Forest()
     forest.add(tree_star)
     forest.add(rock)
     forest.add(tree_o)
+    forest.add(trunk)
 
     print(forest)
