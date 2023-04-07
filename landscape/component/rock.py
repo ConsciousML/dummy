12aacf40faa617bee35cf8a0dd3c1574cc180570
Axel Mendoza <axel.mendoza@epita.fr>
2023-04-07 11:20:12 +0200

    add docstring to forest and components

--- landscape/component/rock.py
+++ landscape/component/rock.py
@@ -1,11 +1,20 @@
+"""Rock component module."""
 from landscape.component.abstract import Component
 
 class Rock(Component):
-    def __init__(self, width, height, char):
+    """This a class to represent a rock component of the landscape"""
+    def __init__(self, width: int, height: int, char: str):
+        """
+        Args:
+            width (int): width of the rock.
+            height (int): height of the rock.
+            char (str): character to print the rock.
+        """
         super().__init__(width, char)
         self.height = height
 
     def __str__(self) -> str:
+        """Return a string representation of the rock"""
         rock_str = ''
         for _ in range(self.height):
             for _ in range(self.width):
